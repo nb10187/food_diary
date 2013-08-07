6e85502751c731b8ffae50f4e4638fdea31e722f
joelfischer <jef@cs.nott.ac.uk>
2013-08-07 14:42:57 +0100

    changed db settigns

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'food_db4',                      # Or path to database file if using sqlite3.
+        'NAME': 'food_diary_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'admin',
-        'PASSWORD': 'password',
+        'USER': 'food_diary',
+        'PASSWORD': 'f00d5',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
