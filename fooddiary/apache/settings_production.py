54f59ede6948f513e0b0c8de14091e827e38b04e
joelfischer <jef@cs.nott.ac.uk>
2013-08-07 15:37:47 +0100

    modified urls scope

--- fooddiary/apache/settings_production.py
+++ fooddiary/apache/settings_production.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'diary.apache.urls_production'
+ROOT_URLCONF = 'fooddiary.apache.urls_production'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
