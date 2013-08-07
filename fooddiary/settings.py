ebd601d0ce53ef2a7908b7b7cb14c24563f96ff5
nb10187 <nuwakl@icloud.com>
2013-08-07 17:24:59 +0100

    commit settings

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -50,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = 'C:/projects/food/bootstrap/'
+MEDIA_ROOT = '/var/www/foody/food_diary/bootstrap/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/jef/foody/static/',
+	'/var/www/foody/food_diary/bootstrap/',
 )
 
 # List of finder classes that know how to find static files in
