1827c9a5847a26db567b2b668016a09e83cb2068
nb10187 <nuwakl@icloud.com>
2013-08-08 16:20:31 +0100

    Update settings_production.py

--- fooddiary/apache/settings_production.py
+++ fooddiary/apache/settings_production.py
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/jef/foody/static/'
+STATIC_ROOT = '/home/nuwan/food/bootstrap/' #'/home/jef/foody/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
