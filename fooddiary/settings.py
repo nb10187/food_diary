159c3cbb3ef869aef8bdf89c6ab2b2772eb0f294
nb10187 <nuwakl@icloud.com>
2013-08-08 10:41:12 +0100

    Update settings.py

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -71,8 +71,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-	'/var/www/foody/food_diary/bootstrap/',
+    # Don't forget to use absolute paths, not relative paths./var/www/foody
+	'/food_diary/bootstrap/',
 )
 
 # List of finder classes that know how to find static files in
