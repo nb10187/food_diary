7cef153ab5ff27b4ed92149817fe7618f5916c00
nb10187 <nuwakl@icloud.com>
2013-08-08 10:25:58 +0100

    Update settings_production.py

--- fooddiary/apache/settings_production.py
+++ fooddiary/apache/settings_production.py
@@ -64,7 +64,7 @@ STATIC_ROOT = '/home/jef/foody/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'var/www/foody/food_diary/bootstrap/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
