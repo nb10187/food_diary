964f855d2e84459946d7574343933dfaaffe330b
nb10187 <nuwakl@icloud.com>
2013-08-09 10:14:12 +0100

    Update settings_production.py

--- fooddiary/apache/settings_production.py
+++ fooddiary/apache/settings_production.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/jef/foody'#os.path.join(ROOT_PATH, 'media')
+MEDIA_ROOT = '/var/www/foody/food_diary/bootstrap/uploaded_files'#'/home/jef/foody'#os.path.join(ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
