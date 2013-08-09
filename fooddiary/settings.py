9c95f39294b2b6dcdac0f570602c6cadb0d097ce
nb10187 <nuwakl@icloud.com>
2013-08-09 10:08:29 +0100

    Update settings.py

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -50,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/var/www/foody/food_diary/bootstrap/'
+MEDIA_ROOT = '/home/jef/foody/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
